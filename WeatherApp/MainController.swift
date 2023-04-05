fa3e25cc79333aeeac12edc66a97e466cbf496ef
Chetan Godhani <chetangodhani9@gmail.com>
2023-04-05 13:58:54 -0400

    Fix: Rewrite annotation function and class, added weather api, json parser, weather icon dictionary

--- WeatherApp/MainController.swift
+++ WeatherApp/MainController.swift
@@ -29,8 +29,8 @@ class MainController: UIViewController {
         locationManager.delegate = self
     }
 
-    private func addCurrentLocationAnnotation(location: CLLocation) {
-        let annotation = MyAnnotation(coordinate: location.coordinate, title: "Temperature 34°F", subtitle: "My Location")
+    private func addLocationAnnotation(location: CLLocationCoordinate2D, weatherData: WeatherResponse) {
+        let annotation = MyAnnotation(coordinate: location, weatherData: weatherData)
         
         mapView.addAnnotation(annotation)
     }
@@ -46,7 +46,7 @@ class MainController: UIViewController {
             return
         }
         
-        addCurrentLocationAnnotation(location: location)
+        loadWeather(search: "\(location.coordinate.latitude),\(location.coordinate.longitude)", location: location.coordinate)
 
         let radiusInMeters: CLLocationDistance = 1000
         
@@ -60,6 +60,76 @@ class MainController: UIViewController {
         // control zooming
         let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 100000)
         mapView.setCameraZoomRange(zoomRange, animated: true)
+    }
+    
+    private func getUrlWith(query: String) -> URL? {
+        let baseUrl = "https://api.weatherapi.com"
+        let endPoint = "/v1/current.json"
+        let key = "9069e104d6a04c4d959172320231503"
+        let airQualityParam = "aqi=no"
+        
+        guard let url = "\(baseUrl)\(endPoint)?key=\(key)&q=\(query)&\(airQualityParam)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return nil
+        }
+
+        return URL(string: url)
+    }
+
+    private func parseJson(data: Data) -> WeatherResponse? {
+        let jsonDecoder = JSONDecoder()
+        var weather: WeatherResponse?
+        do {
+            weather = try jsonDecoder.decode(WeatherResponse.self, from: data)
+        } catch {
+            print(error)
+        }
+        
+        return weather
+    }
+    
+    private func loadWeather(search: String?, location: CLLocationCoordinate2D? = nil) {
+        guard let search = search, !search.isEmpty else { return }
+
+        // Step 1: Get URL
+        guard let url: URL = getUrlWith(query: search) else {
+            print("Could not get url")
+            return
+        }
+
+        // Step 2: Create Session
+        let urlSession = URLSession.shared
+        
+        // Step 3: Create task for session
+        let dataTask = urlSession.dataTask(with: url) { [self] data, response, error in
+            guard error == nil else {
+                print("Error occured:")
+                print(error ?? "")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data found")
+                return
+            }
+            
+            guard let _ = String(data: data, encoding: .utf8) else {
+                print("No data string")
+                return
+            }
+
+            if let weatherResponse = self.parseJson(data: data) {
+                DispatchQueue.main.async {
+                    if let location = location {
+                        self.addLocationAnnotation(location: location, weatherData: weatherResponse)
+                    } else {
+                        let newLocation = CLLocationCoordinate2D(latitude: weatherResponse.location.lat, longitude: weatherResponse.location.lon)
+                        self.addLocationAnnotation(location: newLocation, weatherData: weatherResponse)
+                    }
+                }
+            }
+        }
+        
+        dataTask.resume()
     }
 }
 
@@ -88,8 +158,8 @@ extension MainController: MKMapViewDelegate {
             view.rightCalloutAccessoryView = button
             
             // add an image to left side of callout
-            let image = UIImage(systemName: "graduationcap.circle.fill")
-            view.leftCalloutAccessoryView = UIImageView(image: image)
+            // let image = UIImage(systemName: "graduationcap.circle.fill")
+            // view.leftCalloutAccessoryView = UIImageView(image: image)
             
             // change color of pin
             view.markerTintColor = .systemBlue
@@ -100,6 +170,9 @@ extension MainController: MKMapViewDelegate {
             if let annotation = annotation as? MyAnnotation {
                 // change marker glyph
                 view.glyphText = annotation.glyphText
+                let imageView = UIImageView(image: annotation.weatherImage)
+                imageView.preferredSymbolConfiguration = annotation.symbolConfiguration
+                view.leftCalloutAccessoryView = imageView
             }
         }
         
@@ -130,9 +203,6 @@ extension MainController: CLLocationManagerDelegate {
                 currentLocationAdded = true
                 setupMap()
             }
-            
-//            let latitude = location.coordinate.latitude
-//            let longitude = location.coordinate.longitude
         }
     }
     
@@ -146,13 +216,53 @@ class MyAnnotation: NSObject, MKAnnotation {
     var title: String?
     var subtitle: String?
     var glyphText: String?
+    var weatherImage: UIImage?
+    var symbolConfiguration: UIImage.SymbolConfiguration?
     
-    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String, glyphText: String? = nil) {
+    init(coordinate: CLLocationCoordinate2D, weatherData: WeatherResponse) {
+        let tempC = "\(Int(weatherData.current.temp_c.rounded(.toNearestOrEven)))°C"
+        let feelLikeC = "\(Int(weatherData.current.feelslike_c.rounded(.toNearestOrEven)))°C"
+        let weatherCondition = weatherData.current.condition.text
+        let weatherCode = weatherData.current.condition.code
+        let weatherImage = weatherIconDictionary[weatherCode]?.generateNightImage()
+        
+        if (weatherData.current.is_day == 0) {
+            self.symbolConfiguration = UIImage.SymbolConfiguration(paletteColors: [.white, .white])
+        } else {
+            self.symbolConfiguration = UIImage.SymbolConfiguration(paletteColors: [.systemBlue, UIColor(red: 1.00, green: 0.65, blue: 0.00, alpha: 1.00)])
+        }
+        
         self.coordinate = coordinate
-        self.title = title
-        self.subtitle = subtitle
-        self.glyphText = glyphText
+        self.title = weatherCondition
+        self.subtitle = "Temp: \(tempC), Feels like: \(feelLikeC)"
+        self.glyphText = tempC
+        self.weatherImage = weatherImage
         
         super.init()
     }
+}
+
+struct WeatherResponse: Decodable {
+    let location: Location
+    let current: Current
+}
+
+struct Location: Decodable {
+    let name: String
+    let region: String
+    let country: String
+    let lat: Double
+    let lon: Double
+}
+
+struct Current: Decodable {
+    let temp_c: Float
+    let condition: WeatherCondition
+    let is_day: Int
+    let feelslike_c: Float
+}
+
+struct WeatherCondition: Decodable {
+    let text: String
+    let code: Int
 }
